b1810751dde51e5826c9519cfc7fb21843ad2752
David Laprade <david.laprade@gmail.com>
2016-09-08 15:43:46 -0400

    Test NetSuite.Rest.Roles.get/2

---
+++ test/lib/rest_roles_test.exs
@@ -0,0 +1,27 @@
+defmodule TestHelper.HTTP.Client do
+  @behaviour NetSuite.HTTP
+  def get(uri, headers), do: "response"
+  def parse(response), do: "parsed #{response}"
+end
+
+defmodule NetSuiteRestRolesTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    NetSuite.config %{
+      email: "fake email",
+      password: "secret",
+      production: true
+    }
+  end
+
+  test "it gets and parses the API response" do
+    roles_response = NetSuite.Rest.Roles.get(
+      NetSuite.config,
+      TestHelper.HTTP.Client
+    )
+
+    assert roles_response == "parsed response"
+  end
+end
+
