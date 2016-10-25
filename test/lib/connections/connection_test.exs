0663701ac85d26835ebc893a1d7f91ece8d13068
David Laprade <david.laprade@gmail.com>
2016-10-24 23:06:21 -0400

    Expand NetSuite.Connections.Connection test for async requests

--- test/lib/connections/connection_test.exs
+++ test/lib/connections/connection_test.exs
@@ -19,6 +19,32 @@ defmodule NetSuite.Connections.ConnectionTest do
     assert @described_module.get_config(agent) == %{my: :config}
   end
 
+  test "runs cast functions asynchronously", %{agent: agent} do
+    assert :ok == @described_module.cast(agent, make_ref(), fn(_)->
+      :timer.sleep(1000000000000000000000000000)
+    end)
+  end
+
+  test "notifies event reciever when casting functions", %{agent: agent} do
+    assert :ok == @described_module.cast(agent, ticket=make_ref(), fn(_)->
+      :timer.sleep(1000000000000000000000000000)
+    end)
+    assert {:pending, nil} == NetSuite.Connections.Receiver.get(ticket)
+  end
+
+  test "doesn't crash when there are errors in netsuite_calls", %{agent: agent} do
+    buggy_funct = fn(_) -> 2+nil end
+    assert :ok == @described_module.cast(agent, ticket=make_ref(), buggy_funct)
+    assert {:error, %ArithmeticError{message: "bad argument in arithmetic expression"}} == wait_for_response(ticket)
+    assert Process.alive?(agent)
+  end
+
+  test "notifies event reciever when cast functions complete", %{agent: agent} do
+    assert :ok == @described_module.cast(agent, ticket=make_ref(), fn(_)-> 42 end)
+
+    assert {:ok, 42} == wait_for_response(ticket)
+  end
+
   test "updates configs", %{agent: agent} do
     @described_module.update_config(agent, %{new: :config})
     assert @described_module.get_config(agent) == %{new: :config}
