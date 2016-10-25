ee03bd90a0b2de64581e63f81ba085935ab8def5
David Laprade <david.laprade@gmail.com>
2016-10-24 23:05:02 -0400

    Let NetSuite.Connections make async API calls, yielding configs

--- lib/netsuite_elixir/connections/connection.ex
+++ lib/netsuite_elixir/connections/connection.ex
@@ -16,4 +16,27 @@ defmodule NetSuite.Connections.Connection do
 
   def get_config(pid), do: Agent.get(pid, &(&1))
 
+  @doc """
+    Makes the NS API call asynchronously, exposing the connection's
+    configuration as a callback. Notifies the Receiver when it
+    starts and finishes requests
+  """
+  def cast(pid, ticket, netsuite_call) when
+    is_pid(pid) and
+    is_reference(ticket) and
+    is_function(netsuite_call) do
+
+    GenEvent.notify(@receiver, {:begin_request, ticket})
+
+    Agent.cast(pid, fn(config)->
+      response = try do
+        {:finished, netsuite_call.(config)}
+      rescue
+        error in _ -> {:error, error}
+      end
+      GenEvent.notify(@receiver, {:end_request, {ticket, response}})
+      config
+    end)
+  end
+
 end
