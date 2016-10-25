a05e52ea734f74746e6f2f784b820ee2382ad18c
David Laprade <david.laprade@gmail.com>
2016-10-24 22:40:18 -0400

    Add OTP convenience methods to application module

--- lib/netsuite_elixir.ex
+++ lib/netsuite_elixir.ex
@@ -1,12 +1,6 @@
 defmodule NetSuite do
   use Application
 
-  defdelegate config(auth), to: NetSuite.Configuration, as: :set
-  defdelegate config, to: NetSuite.Configuration, as: :get
-
-  defdelegate call(funct), to: NetSuite.Connections.Pool, as: :queue
-  defdelegate get(ticket), to: NetSuite.Connections.Pool, as: :response
-
   def start(_type, _args) do
     {:ok, sup}  = NetSuite.Connections.Supervisor.start_link
 
@@ -20,4 +14,42 @@ defmodule NetSuite do
     {:ok, sup}
   end
 
+  defdelegate config(auth), to: NetSuite.Configuration, as: :set
+  defdelegate config, to: NetSuite.Configuration, as: :get
+
+  @doc """
+  Make a NetSuite call asynchronously using the next connection in the pool
+  """
+  @spec call_async(fun) :: {atom, reference}
+  defdelegate call_async(funct), to: NetSuite.Connections.Pool, as: :queue
+
+  @spec get(reference) :: {atom, any}
+  defdelegate get(ticket), to: NetSuite.Connections.Pool, as: :response
+
+  @doc """
+  Make a NetSuite call using the next connection in the connection pool
+  Block the current process until the call finishes
+
+  Returns a List, the first member of which is a status code:
+    :ok      -> the request was made and ran without error
+    :error   -> there was an error processing the request
+    :pending -> the request is still being made, check back later
+
+  The second member of the return value depends on the first:
+    :ok      -> the return value of the netsuite_call passed in
+    :error   -> an error description or struct
+    :pending -> nil
+  """
+  @spec call(fun) :: {atom, any}
+  def call(netsuite_call) when is_function(netsuite_call) do
+    {:ok, ticket} = call_async(netsuite_call)
+    wait_for_response(ticket)
+  end
+
+  defp wait_for_response(ticket) do
+    case get(ticket) do
+      {:pending, _response} -> wait_for_response(ticket)
+      response              -> response
+    end
+  end
 end
