53e5ffabe1c6e9132d1013003d258835a26cc911
David Laprade <david.laprade@gmail.com>
2016-10-24 22:59:48 -0400

    Define application start behavior

--- lib/netsuite_elixir.ex
+++ lib/netsuite_elixir.ex
@@ -1,6 +1,17 @@
 defmodule NetSuite do
+  use Application
 
   defdelegate config(auth), to: NetSuite.Configuration, as: :set
   defdelegate config, to: NetSuite.Configuration, as: :get
+
+  def start(_type, _args) do
+    {:ok, sup}  = NetSuite.Connections.Supervisor.start_link
+
+    {:ok, _pid} = NetSuite.Connections.Pool.start_link
+    configs = Application.get_env(:netsuite_elixir, :configs, [])
+    for config <- configs, do: NetSuite.Connections.Pool.add(config)
+
+    {:ok, sup}
+  end
 
 end
