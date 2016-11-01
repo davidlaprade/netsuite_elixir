4c5cd7b5b1a4f38bafce675b51a7aa5e0f09ac7c
David Laprade <david.laprade@gmail.com>
2016-10-31 21:31:37 -0400

    Refactor connection test given NetSuite#wait_for_response/1

--- test/lib/connections/connection_test.exs
+++ test/lib/connections/connection_test.exs
@@ -3,13 +3,6 @@ defmodule NetSuite.Connections.ConnectionTest do
 
   @described_module NetSuite.Connections.Connection
 
-  defp wait_for_response(ticket) do
-    case NetSuite.Connections.Receiver.get(ticket) do
-      {:pending, nil} -> wait_for_response(ticket)
-      response      -> response
-    end
-  end
-
   setup do
     {:ok, pid} = @described_module.start_link(%{my: :config})
     {:ok, %{agent: pid}}
@@ -18,6 +11,8 @@ defmodule NetSuite.Connections.ConnectionTest do
   test "stores configs", %{agent: agent} do
     assert @described_module.get_config(agent) == %{my: :config}
   end
+
+  test "can cast multiple functions to the same agent"
 
   test "runs cast functions asynchronously", %{agent: agent} do
     assert :ok == @described_module.cast(agent, make_ref(), fn(_)->
@@ -35,14 +30,15 @@ defmodule NetSuite.Connections.ConnectionTest do
   test "doesn't crash when there are errors in netsuite_calls", %{agent: agent} do
     buggy_funct = fn(_) -> raise ArithmeticError end
     assert :ok == @described_module.cast(agent, ticket=make_ref(), buggy_funct)
-    assert {:error, %ArithmeticError{message: "bad argument in arithmetic expression"}} == wait_for_response(ticket)
+    assert {:error, %ArithmeticError{message: "bad argument in arithmetic
+    expression"}} == NetSuite.wait_for_response(ticket)
     assert Process.alive?(agent)
   end
 
   test "notifies event reciever when cast functions complete", %{agent: agent} do
     assert :ok == @described_module.cast(agent, ticket=make_ref(), fn(_)-> 42 end)
 
-    assert {:ok, 42} == wait_for_response(ticket)
+    assert {:ok, 42} == NetSuite.wait_for_response(ticket)
   end
 
   test "updates configs", %{agent: agent} do
