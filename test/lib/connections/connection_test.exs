1f5bf7950fb58603acc2a7e924a840093ee79f51
David Laprade <david.laprade@gmail.com>
2016-11-01 10:54:16 -0400

    Test that connections can take multiple requests before they finish one
    Connections need to be able to handle their own queue of netsuite_calls

--- test/lib/connections/connection_test.exs
+++ test/lib/connections/connection_test.exs
@@ -12,12 +12,34 @@ defmodule NetSuite.Connections.ConnectionTest do
     assert @described_module.get_config(agent) == %{my: :config}
   end
 
-  test "can cast multiple functions to the same agent"
-
   test "runs cast functions asynchronously", %{agent: agent} do
     assert :ok == @described_module.cast(agent, make_ref(), fn(_)->
       :timer.sleep(1000000000000000000000000000)
     end)
+  end
+
+  defp count_requests_for(agent) do
+    Enum.count(NetSuite.Connections.Receiver.engaged_connections, &(&1==agent))
+  end
+
+  test "can cast repeatedly to agent without running what's cast", %{agent: agent} do
+    sleep = fn(_)-> :timer.sleep(10000) end
+    assert count_requests_for(agent) == 0
+
+    assert :ok == @described_module.cast(agent, ref1=make_ref(), sleep)
+    assert NetSuite.Connections.Receiver.get(ref1) == {:pending, nil}
+    assert count_requests_for(agent) == 1
+
+    assert :ok == @described_module.cast(agent, ref2=make_ref(), sleep)
+    assert NetSuite.Connections.Receiver.get(ref1) == {:pending, nil}
+    assert NetSuite.Connections.Receiver.get(ref2) == {:pending, nil}
+    assert count_requests_for(agent) == 2
+
+    assert :ok == @described_module.cast(agent, ref3=make_ref(), sleep)
+    assert NetSuite.Connections.Receiver.get(ref1) == {:pending, nil}
+    assert NetSuite.Connections.Receiver.get(ref2) == {:pending, nil}
+    assert NetSuite.Connections.Receiver.get(ref3) == {:pending, nil}
+    assert count_requests_for(agent) == 3
   end
 
   test "notifies event reciever when casting functions", %{agent: agent} do
